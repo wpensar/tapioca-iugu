62958644ee982cf4c7b0d9a76af4fc5cb27ae9ca
Allisson Azevedo <allisson@gmail.com>
2016-05-31 10:24:29 -0300

    Versão 0.1.1

--- tapioca_iugu/__init__.py
+++ tapioca_iugu/__init__.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 
-__author__ = 'Allisson Azevedo'
-__email__ = 'allisson@gmail.com'
-__version__ = '0.1.0'
+__author__ = 'Olist Developers'
+__email__ = 'developers@olist.com'
+__version__ = '0.1.1'
 
 
 from .tapioca_iugu import Iugu  # noqa
